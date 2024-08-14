fa4ad4f8360d4071eaa21450aec65be87d2f0159
dengxs <1195279222@qq.com>
2024-08-14 17:35:14 +0800

    word转pdf aspose-words--controller可以选择返回文件流或者json格式

--- src/main/java/com/silence/watermarkdemo/controller/TestController.java
+++ src/main/java/com/silence/watermarkdemo/controller/TestController.java
@@ -3,12 +3,18 @@ package com.silence.watermarkdemo.controller;
 import com.silence.watermarkdemo.utils.FileRemarkUtil;
 import com.silence.watermarkdemo.utils.Word2PdfUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLEncoder;
@@ -55,22 +61,41 @@ public class TestController {
     }
 
     @PostMapping("/wordToPdf")
-    public void wordToPdf(HttpServletResponse response, MultipartFile file){
+    @ResponseBody
+    public ResponseEntity<?> wordToPdf(HttpServletResponse response, MultipartFile file){
         // 将文件流拷贝到输出流
         InputStream fileInputStream = null;
+        int i = 0;
         try{
             fileInputStream = file.getInputStream();
-            response.setContentType("application/octet-stream");
+//            ServletOutputStream outputStream = response.getOutputStream();
+//            if(i==0){
+//            response.setContentType("application/octet-stream");
             String encodedFileName = URLEncoder.encode(file.getOriginalFilename(), "UTF-8")
                     .replaceAll("\\+", "%20");
-
-            response.setHeader("Content-Disposition", "attachment; filename*=UTF-8''" + encodedFileName);
+//
+//            response.setHeader("Content-Disposition", "attachment; filename*=UTF-8''" + encodedFileName);
 
 //            Documents4jUtil.convertWordToPdf(fileInputStream,response.getOutputStream());
 //            Documents4jUtil.test_word_to_pdf(fileInputStream,response.getOutputStream());
 //            myTest.wordToPdfByJod(fileInputStream,response.getOutputStream(),encodedFileName);
-            Word2PdfUtil.doc2pdf(fileInputStream, response.getOutputStream());
-            response.flushBuffer();
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                Word2PdfUtil.doc2pdf(fileInputStream, byteArrayOutputStream);
+
+
+//            response.flushBuffer();
+                if(encodedFileName.contains(".docx")){
+                    encodedFileName = encodedFileName.replace(".docx", ".pdf");
+                }
+                if (encodedFileName.contains(".doc")){
+                    encodedFileName = encodedFileName.replace(".doc", ".pdf");
+                }
+                return ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename*=UTF-8''" + encodedFileName + "\"")
+                        .body(byteArrayOutputStream.toByteArray());
+//            }
+
             //更新文件下载次数
 
         }catch (Exception e){
@@ -84,6 +109,7 @@ public class TestController {
             }
         }
 
+        return ResponseEntity.internalServerError().contentType(MediaType.APPLICATION_JSON).body("fail");
     }
 
 
